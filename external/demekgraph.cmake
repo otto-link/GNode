d2d2b87cd7551d15b657fa7d14f6ffab137b403a
Otto Link <otto.link.bv@gmail.com>
2024-03-10 20:46:01 +0100

    Fix external dependencies target name to avoid conflicts

--- external/demekgraph.cmake
+++ external/demekgraph.cmake
@@ -3,6 +3,6 @@ project(demekgraph)
 set(DMK_DIR ${CMAKE_CURRENT_SOURCE_DIR}/demekgraph/updated/include)
 
 add_library(${PROJECT_NAME} INTERFACE)
-add_library(${PROJECT_NAME}::${PROJECT_NAME} ALIAS ${PROJECT_NAME})
+add_library(gnode::${PROJECT_NAME} ALIAS ${PROJECT_NAME})
 
 target_include_directories(${PROJECT_NAME} INTERFACE ${DMK_DIR})
