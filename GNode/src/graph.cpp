3e8ef0feed6869f4b12bece2ded43fbbd88a8b89
Otto Link <otto.link.bv@gmail.com>
2024-09-14 17:16:57 +0200

    Fix github workflow

--- GNode/src/graph.cpp
+++ GNode/src/graph.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <sstream>
 
 #include "demekgraph/updated/include/graph.hpp"
 #include "demekgraph/updated/include/layout.hpp"
