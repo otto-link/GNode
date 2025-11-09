37769dd2cfd31f95bbfa07294c3a58625c7d1328
otto-link <otto-link@users.noreply.github.com>
2025-11-09 20:46:39 +0000

    deploy: f49f54182b8023c239e79fcf3d73f0808ed31cbd

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var index =
 [
     [ "Introduction", "index.html#intro_sec", null ],
+    [ "Installation", "index.html#install", null ],
     [ "Usage Example", "index.html#usage_sec", null ],
     [ "Repository", "index.html#repo_sec", null ]
 ];
